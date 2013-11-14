610ba00f91f3c1893708b5ebc4e7b56391ec90bd
Scott Albertine <scott.albertine@jumptap.com>
2013-11-14 17:54:31 -0500

    a fuller skeleton of the lexer

--- src/com/millennialmedia/intellibot/psi/RobotLexer.java
+++ src/com/millennialmedia/intellibot/psi/RobotLexer.java
@@ -1,62 +1,99 @@
 package com.millennialmedia.intellibot.psi;
 
-import com.intellij.lexer.Lexer;
-import com.intellij.lexer.LexerPosition;
+import java.util.*;
+
+import com.intellij.lexer.LexerBase;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.util.ArrayUtil;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * @author: Stephen Abrams
- */
-public class RobotLexer extends Lexer {
+public class RobotLexer extends LexerBase {
+
+    protected CharSequence myBuffer = ArrayUtil.EMPTY_CHAR_SEQUENCE;
+    protected int myStartOffset = 0;
+    protected int myEndOffset = 0;
+    private int myPosition;
+    private IElementType myCurrentToken;
+    private int myCurrentTokenStart;
+    private List<String> myKeywords;
+    private int myState;
+
+    private final RobotKeywordProvider myKeywordProvider;
+    private String myCurLanguage;
+
+    public final static int STATE_DEFAULT = 0;
+    public final static int STATE_INSIDE_GARBAGE = 1;
+    public final static int STATE_INSIDE_KEYWORD = 2;
+    public final static int STATE_INSIDE_VARIABLE = 3;
+    public final static int STATE_DONE = 4;
+
+    public RobotLexer(RobotKeywordProvider provider) {
+        myKeywordProvider = provider;
+        updateLanguage("en");
+    }
+
+    private void updateLanguage(String language) {
+        myCurLanguage = language;
+        myKeywords = new ArrayList<String>(myKeywordProvider.getAllKeywords(language));
+        Collections.sort(myKeywords, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
+    }
+
     @Override
     public void start(CharSequence buffer, int startOffset, int endOffset, int initialState) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        myBuffer = buffer;
+        myStartOffset = startOffset;
+        myEndOffset = endOffset;
+        myPosition = startOffset;
+        myState = initialState;
+        advance();
+    }
+
+    @Nullable
+    public static String fetchLocationLanguage(final @NotNull String commentText) {
+        if (commentText.startsWith("language:")) {
+            return commentText.substring(9).trim();
+        }
+        return null;
+    }
+
+    @Override
+    public void advance() {
+
     }
 
     @Override
     public int getState() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return myState;
     }
 
     @Nullable
     @Override
     public IElementType getTokenType() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return myCurrentToken;
     }
 
     @Override
     public int getTokenStart() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return myCurrentTokenStart;
     }
 
     @Override
     public int getTokenEnd() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void advance() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public LexerPosition getCurrentPosition() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void restore(LexerPosition position) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        return myPosition;
     }
 
     @Override
     public CharSequence getBufferSequence() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return myBuffer;
     }
 
     @Override
     public int getBufferEnd() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return myEndOffset;
     }
 }
