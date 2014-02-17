b4f00096fc44e3fd0dd95423d25d67dc2b96c68d
mrubino <mrubino@millennialmedia.com>
2014-02-17 11:21:38 -0500

    limit the availability of some recommendations. Left keyword importing alone as they seem to be users a bit all over.

--- src/com/millennialmedia/intellibot/ide/RobotCompletionContributor.java
+++ src/com/millennialmedia/intellibot/ide/RobotCompletionContributor.java
@@ -7,15 +7,18 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.codeInsight.lookup.TailTypeDecorator;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.ProcessingContext;
 import com.millennialmedia.intellibot.psi.RecommendationWord;
 import com.millennialmedia.intellibot.psi.RobotElementType;
 import com.millennialmedia.intellibot.psi.RobotKeywordProvider;
 import com.millennialmedia.intellibot.psi.RobotTokenTypes;
+import com.millennialmedia.intellibot.psi.element.Heading;
 import com.millennialmedia.intellibot.psi.element.KeywordFile;
 import com.millennialmedia.intellibot.psi.element.RobotFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
@@ -48,57 +51,114 @@ public class RobotCompletionContributor extends CompletionContributor {
         }
     };
 
+    private static PsiElement getHeading(PsiElement current) {
+        if (current == null) {
+            return null;
+        }
+        if (current instanceof Heading) {
+            return current;
+        } else {
+            return getHeading(current.getParent());
+        }
+    }
+
+    private static boolean isInSettings(@Nullable PsiElement element) {
+        boolean result = false;
+        if (element instanceof Heading) {
+            result = ((Heading) element).isSettings();
+        }
+        return result;
+    }
+
+    private static boolean isInTestCases(@Nullable PsiElement element) {
+        boolean result = false;
+        if (element instanceof Heading) {
+            result = ((Heading) element).containsTestCases();
+        }
+        return result;
+    }
+
+    private static boolean isInKeywords(@Nullable PsiElement element) {
+        boolean result = false;
+        if (element instanceof Heading) {
+            result = ((Heading) element).containsKeywordDefinitions();
+        }
+        return result;
+    }
+
     public RobotCompletionContributor() {
 
+        // This is the rule for adding Headings (*** Settings ***, *** Test Cases ***)
         extend(CompletionType.BASIC,
                 psiElement().inFile(psiElement(RobotFile.class)),
                 new CompletionProvider<CompletionParameters>() {
                     @Override
-                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet results) {
+                    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                                  ProcessingContext context,
+                                                  @NotNull CompletionResultSet results) {
                         addSyntaxLookup(RobotTokenTypes.HEADING, results, NEW_LINE);
                     }
                 });
 
+        // This is the rule for adding Bracket Settings ([Tags], [Setup])
         // TODO: MTR: some brackets are only for Test Cases, some only Keywords, some both
         extend(CompletionType.BASIC,
                 psiElement().inFile(psiElement(RobotFile.class)),
                 new CompletionProvider<CompletionParameters>() {
                     @Override
-                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet results) {
-                        addSyntaxLookup(RobotTokenTypes.BRACKET_SETTING, results, SUPER_SPACE);
+                    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                                  ProcessingContext context,
+                                                  @NotNull CompletionResultSet results) {
+                        PsiElement heading = getHeading(parameters.getOriginalPosition());
+                        if (isInTestCases(heading) || isInKeywords(heading)) {
+                            addSyntaxLookup(RobotTokenTypes.BRACKET_SETTING, results, SUPER_SPACE);
+                        }
                     }
                 });
 
+        // This is the rule for adding settings and imports (Library, Test Setup)
         // TODO: MTR: Settings and Imports should only be in *** Settings ***
         extend(CompletionType.BASIC,
                 psiElement().inFile(psiElement(RobotFile.class)),
                 new CompletionProvider<CompletionParameters>() {
                     @Override
-                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet results) {
-                        addSyntaxLookup(RobotTokenTypes.SETTING, results, SUPER_SPACE);
-                        addSyntaxLookup(RobotTokenTypes.IMPORT, results, SUPER_SPACE);
+                    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                                  ProcessingContext context,
+                                                  @NotNull CompletionResultSet results) {
+                        if (isInSettings(getHeading(parameters.getOriginalPosition()))) {
+                            addSyntaxLookup(RobotTokenTypes.SETTING, results, SUPER_SPACE);
+                            addSyntaxLookup(RobotTokenTypes.IMPORT, results, SUPER_SPACE);
+                        }
                     }
                 });
 
+        // This is the rule for adding Gherkin (When, Then)
         // TODO: MTR: only in Test Cases
         extend(CompletionType.BASIC,
                 psiElement().inFile(psiElement(RobotFile.class)),
                 new CompletionProvider<CompletionParameters>() {
                     @Override
-                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet results) {
-                        addSyntaxLookup(RobotTokenTypes.GHERKIN, results, TailType.SPACE);
+                    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                                  ProcessingContext context,
+                                                  @NotNull CompletionResultSet results) {
+                        if (isInTestCases(getHeading(parameters.getOriginalPosition()))) {
+                            addSyntaxLookup(RobotTokenTypes.GHERKIN, results, TailType.SPACE);
+                        }
                     }
                 });
 
+        // This is the rule for adding imported keywords and library methods
         // TODO: MTR: only in test cases and keyword definitions
-        extend(CompletionType.BASIC, psiElement().inFile(psiElement(RobotFile.class)), new CompletionProvider<CompletionParameters>() {
-            @Override
-            protected void addCompletions(@NotNull CompletionParameters parameters,
-                                          ProcessingContext context,
-                                          @NotNull CompletionResultSet result) {
-                addRobotKeywords(result, parameters.getOriginalFile());
-            }
-        });
+        extend(CompletionType.BASIC,
+                psiElement().inFile(psiElement(RobotFile.class)),
+                new CompletionProvider<CompletionParameters>() {
+                    @Override
+                    protected void addCompletions(@NotNull CompletionParameters parameters,
+                                                  ProcessingContext context,
+                                                  @NotNull CompletionResultSet result) {
+                        addRobotKeywords(result, parameters.getOriginalFile());
+                    }
+                });
     }
 
     @Override
